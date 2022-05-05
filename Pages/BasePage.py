2e87f28185baaafff79be13f9cf058d79391eed2
Nenet Gevorgyan <nenet.mukelyan@gmail.com>
2022-05-05 19:51:38 +0400

    Update Commit

--- Pages/BasePage.py
+++ Pages/BasePage.py
@@ -13,23 +13,8 @@ class BasePage:
     def do_send_keys(self, by_locator, text):
         WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(by_locator)).send_keys(text)
 
-    def get_text_from_element(self, by_locator):
-        element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(by_locator))
-        text = element.text
-        return text
-
-    def locate_element_by_selector(self, by_locator):
-        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(by_locator))
-        return 1
-
-    def get_attr_value_from_element(self, by_locator, attr_name):
-        element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(by_locator))
-        value = element.get_attribute(attr_name)
-        return value
-
     def navigate_by_url(self, url):
         self.driver.get(url)
 
     def do_register(self, username, email, address1, address2):
         pass
-
